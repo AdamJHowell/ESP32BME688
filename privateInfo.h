2ead8241710d5892549edd3d766cf2f81de5fe0a
Adam Howell <AdamJHowell@users.noreply.github.com>
2022-03-15 00:07:19 -0600

    Delete privateInfo.h

--- privateInfo.h
+++
@@ -1,5 +0,0 @@
-const char* wifiSsid = "Red5";
-const char* wifiPassword = "8012254722";
-const char* mqttBroker = "192.168.55.200";
-const int mqttPort = 1883;
-const char* ThingSpeakWriteKey = "AOD6JO0V1N6IYREJ";
